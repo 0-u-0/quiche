495165ac37178e4b91891b05af2acbbdf4d661a3
chen cong <chencong@100url.com.cn>
2023-05-17 11:33:40 +0000

    fix absl flag

--- main.cc
+++ main.cc
@@ -1,5 +1,10 @@
 #include <iostream>
 
+#include "absl/flags/parse.h"
+#include "absl/flags/usage.h"
+
 int main(int, char**) {
-    std::cout << "Hello, world!\n";
+    // std::cout << "Hello, world!\n";
+    absl::ProgramUsageMessage();
+
 }
